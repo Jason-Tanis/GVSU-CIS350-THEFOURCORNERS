7aac277adc914954eee9a54a3d7d9e5132bde7ca
unknown <tanisjas@mail.gvsu.edu>
2025-02-13 11:38:24 -0500

    Modified app.py file so that the main box for the application is light green

--- src/degree_dollars/src/degree_dollars/app.py
+++ src/degree_dollars/src/degree_dollars/app.py
@@ -8,16 +8,16 @@ from toga.style.pack import COLUMN, ROW
 
 
 class DegreeDollars(toga.App):
-    def startup(self):
+    def startup(self): #Define the app's behavior when it is initially opened
         """Construct and show the Toga application.
 
         Usually, you would add your application to a main content box.
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        main_box = toga.Box(style=Pack(background_color=("#DDFFD6"))) #The box that opens upon startup
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window = toga.MainWindow(title=self.formal_name) #Window in which box is displayed
         self.main_window.content = main_box
         self.main_window.show()
 
