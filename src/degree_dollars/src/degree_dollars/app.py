beb663e7795aa6e69718d0fe1f275045b3d39d4d
Kelsey Tedford <kelsey.tedford@Kelseys-MacBook-Pro-2.local>
2025-03-26 19:43:48 -0400

    Updated database username

--- src/degree_dollars/src/degree_dollars/app.py
+++ src/degree_dollars/src/degree_dollars/app.py
@@ -24,7 +24,7 @@ import math
 config = {
     "host": "localhost",
     "port": 3306,
-    "user": "DegreeDollarsApp",
+    "user": "root",
     "password": "DegreeDollars350!"
 }
 MYSQL_DATABASE = "DegreeDollars"
